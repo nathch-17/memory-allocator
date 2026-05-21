7170977cf05a04b3a6fa72698f8c55762ec9ed62
Nathan <chaignonnathan@gmail.com>
2026-05-22 00:31:11 +0200

    ajout de la fonction split

--- src/my_malloc.c
+++ src/my_malloc.c
@@ -1,21 +1,56 @@
-#include<stdio.h>
-#include"my_malloc.h"
+#include <stdio.h>
+#include <unistd.h> 
 
+#include "my_malloc.h"
 
 static my_stats *global_info = NULL;
 
+void split(area* block, size_t size){
+  
+  area *nouveau_bloc = (area*)((uint8_t*)block+sizeof(area)+size);
+  block -> inUse = true;
+  area* ancien_next = block -> next;
+  uint32_t ancien_length = block-> length;
+  
+  if(ancien_next != NULL ){
+    ancien_next -> prev = nouveau_bloc;
+  }
+    
+  /*si ancien next pas null, son prev doit pointer vers nouveau_bloc et non plus vers block*/
+  block -> next = nouveau_bloc;
 
-/*if (global_info == NULL)
- * init(size) à ajouter dans le malloc plus tard*/
+  block -> length = size;
+  /*block -> prev =  deja fait*/
+  
+  nouveau_bloc -> inUse = false;
+  nouveau_bloc -> prev = block;
+  nouveau_bloc -> length = ancien_length - size - sizeof(area);
+  nouveau_bloc -> next = ancien_next;
+
+
+
+
+
+  
+  
+
+
+
+}
 
 void init(size_t size){
-  size_t total = sizeof(my_stats)+sizeof(area)+size;
+  int page_size = getpagesize();
+
+  /*Réserve une zone de mémoire pour inscrire toute les donées nécessaire pour le bloc global et le petit bloc ainsi que l'epsace demandé par l'utilisateur*/
+  size_t besoin = sizeof(my_stats)+sizeof(area)+size;
+  int nb_pages = (besoin+page_size-1)/page_size;
+  size_t total = nb_pages*page_size;
   void* ptr_1 = sbrk(total);
   
   global_info = (my_stats *)ptr_1;
   global_info -> magical_bytes = 0x42;
   global_info -> amount_of_blocks = 1;
-  global_info -> amount_of_pages = 1;
+  global_info -> amount_of_pages = nb_pages;
 
   area *premier_bloc = (area*)(global_info + 1);
   /*En c si on ajoute 1 a un pointeur vers un structure, il saute exactement la taille de la strcuture*/
@@ -23,7 +58,6 @@ void init(size_t size){
   premier_bloc -> prev = NULL;
   premier_bloc -> next = NULL;
   premier_bloc -> length = total - sizeof(my_stats) - sizeof(area);
-
 }
 
 void *my_malloc(size_t size){
@@ -33,12 +67,47 @@ void *my_malloc(size_t size){
   area *courant = (area*)(global_info+1);
   
   while (courant!= NULL){
-  if(!courant ->inUse && courant->length >=size){
-  courant ->inUse = true;
-  return (void*) (courant+1);
-  }
-  courant = courant->next;
+    
+    if(!courant ->inUse && courant->length >=size){
+     
+      courant ->inUse = true;
+      /*vérfifier si on peut splitter */
+      if(courant -> length >= size + sizeof(area*) +1){
+        split(courant,size);
+      }
+      
+      return (void*) (courant+1); /*renvoi un pointeur vers le début de la zone utilisable du bloc alloué*/
+    }
+
+    area* ptr_prev = courant;
+      if(courant->next == NULL){
+    
+        size_t total = (sizeof(area)+size);
+   
+        void* ptr = sbrk(total);
+   
+        if(ptr == (void *)-1) return NULL;
+    
+
+        area* nouveau_bloc = (area*)(ptr);
+        
+        nouveau_bloc -> next = NULL;
+        nouveau_bloc -> length = total - sizeof(area);
+        nouveau_bloc -> prev = ptr_prev;
+        ptr_prev -> next = nouveau_bloc;
+        
+
+        nouveau_bloc -> inUse = true;
+
+
+        return (void*)(nouveau_bloc+1); /*renvoi pointeur vers la nouvelle zone utilisable */
+
+    
+
+      }
+  courant = courant->next; /*si taille insuffisante passe au prochain bloc*/
+ 
   }
 
-  return NULL;
+return NULL;
 }
--- tests/test_main.c
+++ tests/test_main.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
-#include <unistd.h>   // pour sbrk
-#include <stdint.h>
+#include <my_malloc.h>
 
-int main(void) {
-    void *before = sbrk(0);       // adresse actuelle de la fin du tas
-    printf("Avant sbrk(0): %p\n", before);
+int main(int argc, char * argv[]){
+ void *p1 = my_malloc(10);
+ void *p2 = my_malloc(20);
 
-    intptr_t increment = 4096;       // demander 4096 octets
-    void *ret = sbrk(increment);     // retourne l'ancienne fin du tas
-    if (ret == (void *) -1) {
-        perror("sbrk failed");
-        return 1;
-    }
-
-    printf("sbrk(%ld) a retourne : %p\n", (long)increment, ret);
-    printf("Nouvelle fin du tas (sbrk(0)) : %p\n", sbrk(0));
-
-    /* Visualisation : ret vaut l'adresse avant l'extension ;
-       la nouvelle fin est ret + increment */
-    printf("Verification: ret + increment = %p\n", (void *)((char *)ret + increment));
-    return 0;
+ printf
 }
